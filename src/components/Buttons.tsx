978f2d697c02b1dc229884a81e6718591e3b40c6
GuilhermeOliveira-a <nelson.domiciano.nd@gmail.com>
2022-03-29 18:12:34 -0300

    :construction: Create component Button

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -1,5 +1,10 @@
-export function Button(){
+import '../styles/button.scss';
+
+import { ButtonHTMLAttributes } from 'react' //Importando todos os atributos de um botão HTML padrão para usar no Type ButtonProps
+type ButtonProps= ButtonHTMLAttributes<HTMLButtonElement>
+
+export function Button(props: ButtonProps){
   return(
-      <button>Clique aqui</button>
+      <button className='button'{...props}/>
   );
 }
